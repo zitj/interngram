02d2120d389105e0ec6a509c895b09c276f8d499
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2020-11-18 13:40:26 +0100

    added edit feature

--- scripts/post.js
+++ scripts/post.js
@@ -1,9 +1,13 @@
 const id = new URLSearchParams(window.location.search).get('id');
 const container = document.querySelector('.individualPost');
-// const form = document.querySelector('form');
+const formPanel = document.querySelector('.formPanel');
+const darkBackground = document.querySelector('.darkBackground');
+const form = document.querySelector('form');
+// const editPostBtn = document.querySelector('.editPostBtn');
 
+const closeBtn = document.querySelector('.closeBtn');
 // const deleteBtn = document.querySelector('.delete');
-// const editBtn = document.querySelector('.edit');
+const editBtn = document.querySelector('.edit');
 
 const renderIndividualPost = async () => {
     const res = await fetch('http://localhost:3000/posts/' + id);
@@ -28,9 +32,45 @@ const renderIndividualPost = async () => {
         <a href="${post.meta.url}">${post.meta.url}</a>
       `;
     }
-  
     container.innerHTML = template;
-  
   }
+
+  const uploadInputs = async () =>{
+    const res = await fetch('http://localhost:3000/posts/' + id);
+    const post = await res.json(); 
+    form.title.value = post.title;
+    form.type.value = post.type;
+    form.content.value = post.meta.url;
+}
+
+const changePost = async (e) =>{
+    e.preventDefault();
+    const doc = {
+        title: form.title.value,
+        type: form.type.value,
+        meta:{
+            url: form.content.value
+        }
+    }
+    await fetch('http://localhost:3000/posts/' + id, {
+        method: 'PATCH',
+        body: JSON.stringify(doc),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    window.location.reload(`/post.html?=${id}`);
+}
+
+  editBtn.addEventListener('click', ()=>{
+    formPanel.classList.add('active');
+    uploadInputs();
+  })
+  closeBtn.addEventListener('click', ()=>{
+    formPanel.classList.remove('active');
+  })
+  darkBackground.addEventListener('click', ()=>{
+    formPanel.classList.remove('active');
+  })
+
+  form.addEventListener('submit', changePost);
 
   window.addEventListener('DOMContentLoaded', () => renderIndividualPost());
