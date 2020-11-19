caefdddb8b6c697ea5569f65da506f9332346a9a
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2020-11-19 10:03:03 +0100

    Added signUp and signIn buttons

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const signUpBtn = document.querySelector('.signUpBtn');
+const signInBtn = document.querySelector('.signInBtn');
+
+signUpBtn.addEventListener('click', ()=>{
+    console.log('Would you like to sign up?');
+});
+
+signInBtn.addEventListener('click', ()=>{
+    console.log('Would you like to sign in?');
+});
