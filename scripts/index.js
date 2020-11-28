e14b1e5c4af4582a7c0e6de644dcd7b7b124f03c
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2020-11-28 18:32:30 +0100

    bug fixed on like button

--- scripts/index.js
+++ scripts/index.js
@@ -50,7 +50,6 @@ const createUser = async (e) => {
             'https://images-na.ssl-images-amazon.com/images/I/51zLZbEVSTL._AC_SX466_.jpg',
         password: signUpForm.password.value,
         language: 'english',
-        likedPosts: [],
         bookmarkedPosts: [],
     };
     await fetch('http://localhost:3000/users/', {
